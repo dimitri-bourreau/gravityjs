6d84ea7ebcdfe1da6a2e77abfee739ea4b63739d
Dimitri Bourreau <contact@dimitribourreau.me>
2025-02-05 11:45:31 +0100

    refactor: extract logic into small functions

--- main.js
+++ main.js
@@ -3,20 +3,29 @@ const STARTING_DIV_ID = "start";
 const startingDiv = getStartingDiv(STARTING_DIV_ID);
 
 setInterval(() => {
-  const currentTranslation = startingDiv.style.transform;
-  if (!currentTranslation) startingDiv.style.transform = "translateX(10px)";
-  else {
-    const numberOfPixelsAsString = currentTranslation
-      .replace("translateX(", "")
-      .replace(")", "");
-    const numberOfPixels = parseInt(numberOfPixelsAsString);
-    startingDiv.style.transform = `translateX(${numberOfPixels + 10}px)`;
-  }
-}, 100);
+  moveDivToRight(startingDiv);
+}, 10);
 
 function getStartingDiv(id) {
   const div = document.getElementById(id);
   console.log(document);
   if (!div) throw new Error("Impossible de récupérer la div");
   return div;
+}
+
+function moveDivToRight(startingDiv) {
+  const currentTranslation = startingDiv.style.transform;
+  if (!currentTranslation) startingDiv.style.transform = "translateX(10px)";
+  else {
+    const numberOfPixels = getNumberFromTranslateX(currentTranslation);
+    startingDiv.style.transform = setTranslateFromNumber(numberOfPixels + 1);
+  }
+}
+
+function getNumberFromTranslateX(translate) {
+  return parseInt(translate.replace("translateX(", "").replace(")", ""));
+}
+
+function setTranslateFromNumber(n) {
+  return `translateX(${n}px)`;
 }
